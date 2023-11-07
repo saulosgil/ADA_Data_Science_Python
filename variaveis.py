bc7120f850c1a0ab0b8b19244718847d5aef4103
saulosgil <saulosgil@hotmail.com>
2023-11-07 11:37:02 -0300

    'variaveis'

---
+++ variaveis.py
@@ -0,0 +1,35 @@
+# Variáveis
+# 1. variáveis
+
+idade = 26 # criando uma variável
+
+print(idade)
+
+nome = 'Walisson Silva'
+
+print(nome)
+
+"""
+Tipos de variáveis
+  1. int: números inteiros (ex. 0,5,-1,2000);
+  2. Float: números reais (1.0, -2.7, 3.14);
+  3. bool: valores lógicos (booleanos): True or False
+"""
+
+idade = 26
+altura = 1.77
+nome = 'Walisson Silva'
+estudante = True
+
+# Imprimindo o tipo das variáveis com a função type
+
+print(type(idade))
+print(type(altura))
+print(type(nome))
+print(type(estudante))
+
+# Obtendo dados do usuário e salvando em variíveis
+
+linguagem = input("Qual é a linguagem de programação que você está aprendendo? ")
+
+print('A linguagem que você está estudando é', linguagem)
