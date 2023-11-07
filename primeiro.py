a1e8ba25044dc91e53b0cb1a914c4f821b0b487a
saulosgil <saulosgil@hotmail.com>
2023-11-07 11:12:59 -0300

    Introduzindo o print e o input

---
+++ primeiro.py
@@ -0,0 +1,9 @@
+# Imprimindo Hello World
+print('Hello World!')
+
+# Imprimindo Seja bem vindo ao curso de Python do Coding Tank!
+print('Seja bem-vindo(a) ao curso de Python do Coding Tank!')
+
+# Introduzindo  o input
+input('Qual é a linguagem de programação que você está estudando? ')
+
